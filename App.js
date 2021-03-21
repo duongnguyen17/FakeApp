0343e6ed9049813869d3c2eaaac615a043d5d712
duongtavt17 <duongthptnt@gmail.com>
2021-03-22 03:16:01 +0700

    create app

---
+++ App.js
@@ -0,0 +1,57 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+
+
+  return (
+    <SafeAreaView style={styles.sectionContainer}>
+      <StatusBar barStyle={'dark-content'} />
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+        <View>
+          <Text> test sdkgfjhas gdjfgasdjhgfjhsagdhf </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
