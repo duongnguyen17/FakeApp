34abaf92a8713d4f5de9afa1a28b775f60d18333
duongtavt17 <duongthptnt@gmail.com>
2021-04-24 22:07:48 +0700

    TabBar

--- screens/auth/login.screen.js
+++ screens/auth/login.screen.js
@@ -1,44 +1,84 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react';
+import {connect} from 'react-redux';
+import {View, Text, Button, StyleSheet} from 'react-native';
+import {TextInput} from 'react-native-gesture-handler';
+import {Login} from '../../redux/actions/auth.action';
 
-import { View, Text, Button, StyleSheet } from 'react-native'
-import { TextInput } from 'react-native-gesture-handler'
+function LoginScreen(props) {
+  const [phonenumber, setPhonenumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
 
-function Login(props) {
+  // useEffect(() => {
+  //   return () => {
+  //     setPhonenumber(phonenumber);
+  //     setPassword(password);
+  //     setError(error);
+  //   };
+  // }, [error, phonenumber, password]);
+
+  // const handleLogin = () => {
+  //   Login(phonenumber, password);
+  //   if (error === '') {
+  //     props.navigation.navigate('UserProfileScreen');
+  //   }
+  // };
   return (
     <View style={styles.body}>
+      <TextInput
+        placeholder="Phonenumber"
+        onChangeText={text => {
+          setPhonenumber(text);
+        }}
+      />
 
-
-      <TextInput placeholder="Phone" onChangeText={(text) => { }} />
-
-      <TextInput placeholder="Password" secureTextEntry={true} onChangeText={(pass) => { }} />
-      <Text style={{ color: 'red', alignSelf: 'center' }} >loi hien o day</Text>
-      <Button style={{ backgroundColor: "#1a73e8", marginTop: '50%' }}
-        mode="contained"
+      <TextInput
+        placeholder="Password"
+        secureTextEntry={true}
+        onChangeText={pass => {
+          setPassword(pass);
+        }}
+      />
+      <Text style={{color: 'red', alignSelf: 'center'}}>{error}</Text>
+      <Button
+        style={{backgroundColor: '#1a73e8', marginTop: '50%'}}
         uppercase={false}
-        title='login' 
-        onPress={()=>{props.navigation.navigate("Profile")}}/>
+        title="login"
+        onPress={() => {
+          //handleLogin();
+        }}
+      />
       <Text
-        onPress={() => props.navigation.navigate("Signup")}
+        onPress={() => props.navigation.navigate('SignupScreen')}
         style={{
-          color: "#1a73e8",
+          color: '#1a73e8',
           alignSelf: 'center',
           fontSize: 15,
           fontWeight: 'bold',
-          marginTop: 20
-        }}
-      >Create a new account</Text>
+          marginTop: 20,
+        }}>
+        Create a new account
+      </Text>
     </View>
-  )
+  );
 }
-
-export default Login
+// function mapStateToProp(state) {
+//   return {
+//     phonenumber: state.phonenumber,
+//     password: state.password,
+//     error: state.error,
+//   };
+// }
+// const mapDispatchToProp = {
+//   Login,
+// };
+export default LoginScreen;//connect(mapStateToProp, mapDispatchToProp)(LoginScreen);
 
 const styles = StyleSheet.create({
-
   body: {
     paddingTop: '25%',
     height: '100%',
     padding: 20,
-    backgroundColor: "#ffffff"
+    backgroundColor: '#ffffff',
   },
 });
