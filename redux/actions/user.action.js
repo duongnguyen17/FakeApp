cceca182406668a4be00eae2f1b84682229c2b7d
duongtavt17 <duongthptnt@gmail.com>
2021-05-24 01:06:14 +0700

    userAction done

--- redux/actions/user.action.js
+++ redux/actions/user.action.js
@@ -31,6 +31,60 @@ export const changeUserInfor = (token, data) => async dispatch => {
     console.log(error);
   }
 };
+
+//lấy danh sách bài viết mà người dùng quan tâm
+export const getListInterested = (token, index) => async dispatch => {
+  const taskURI = `${BASE_URL}/user/get_list_interested?token=${token}&index=${index}`;
+  try {
+    const res = await axios.get(taskURI);
+    if (res.data.code === apiConstantsCode.OK) {
+      dispatch(getListInterestedSuccess(res.data.data));
+    } else {
+      throw new Error(res.data.message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//follow người khác
+export const followOther = (token, userId) => async dispatch => {
+  const taskURI = `${BASE_URL}/user/follow_other?token=${token}&userId=${userId}`;
+  try {
+    const res = await axios.post(taskURI);
+    if (res.data.code === apiConstantsCode.OK) {
+      dispatch(followOtherSuccess(res.data.data));
+    } else {
+      throw new Error(res.data.message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+//lấy danh sách những người đã follow
+export const getListFollow = token => async dispatch => {
+  const taskURI = `${BASE_URL}/user/get_list_follow?token=${token}`;
+  try {
+    const res = await axios.post(taskURI);
+    if (res.data.code === apiConstantsCode.OK) {
+      dispatch(getListFollowSuccess(res.data.data));
+    } else {
+      throw new Error(res.data.message);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const logout = token => async dispatch => {
+  const taskURI = `${BASE_URL}/user/logout?token=${token}`;
+  try {
+    await axios.post(taskURI);
+    dispatch(logoutSuccess);
+  } catch (error) {
+    console.log(error);
+  }
+};
 export const getUserInforSuccess = data => ({
   type: userAction.GET_USER_INFOR,
   payload: data,
@@ -39,4 +93,24 @@ export const getUserInforSuccess = data => ({
 export const changeUserInforSuccess = data => ({
   type: userAction.CHANGE_INFOR,
   payload: data,
+});
+
+//lấy danh sách bài viết mà người dùng quan tâm
+export const getListInterestedSuccess = data => ({
+  type: userAction.GET_LIST_INTERESTED,
+  payload: data,
+});
+
+export const followOtherSuccess = data => ({
+  type: userAction.FOLLOW_OTHER,
+  payload: data,
+});
+
+export const getListFollowSuccess = data => ({
+  type: userAction.GET_LIST_FOLLOW,
+  payload: data,
+});
+
+export const logoutSuccess = () => ({
+  type: userAction.LOGOUT,
 });
